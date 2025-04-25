fdbd831383206a18d1d6b4ad58eed772d7399540
HafizZada16 <hafizzada16@gmail.com>
2025-04-25 13:11:45 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,9 @@
+video.addEventListener("click", function video() {
+  alert("Video Menkrep");
+});
+audio.addEventListener("click", function audio() {
+    alert("iiiii takutnye");  
+});
+gambar.addEventListener("click", function video() {
+    alert("Ai Hafiz16");
+}); 
